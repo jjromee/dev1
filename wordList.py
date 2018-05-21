eb010edb5e3a2cfb5fe7240fa79f7a31439f77ab
Juan <juanromero2011@yahoo.com>
2018-05-20 21:04:18 -0500

    added exit if file read in is empty

--- wordList.py
+++ wordList.py
@@ -1,4 +1,7 @@
 #create list of each word in file
 def listOfWords(file):
     words = file.read().split()
+    if len(words) == 0 :
+        print "File contains no values/words. Please try a new file. Exiting"
+        exit(0)
     return words
