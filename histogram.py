60e966954c96b95f49ab9b7bc62ac004b5ac154a
Juan <juanromero2011@yahoo.com>
2018-05-20 20:02:20 -0500

    added printMe() in histogram.py - prints in specific format

--- histogram.py
+++ histogram.py
@@ -1,6 +1,6 @@
 import string
 
-
+#create histogram dictionary holding word and number of occurrences
 def histo(words):
     hist = {}
     for word in words:
@@ -9,6 +9,16 @@ def histo(words):
       hist[word] = hist.get(word, 0) + 1 #count number of occurrences of word, if zero, increments by one, else increment previous num by 1
     return hist
 
+
+#takes in dictionary and sorts from greatest value to least
 def sortHisto(hist):
     sortedHist = sorted(hist.items(), key = lambda x: x[1], reverse = True)
     return sortedHist
+
+#print the word followed by a pipe character ("|"),
+#a number of equal signs that are proportional to the
+#number of occurrences found in the text, and the number of occurrences itself.
+def printMe(sortedHist):
+    for key, value in sortedHist:
+        print key, '|', ('=' * value), '({})'.format(value)
+
