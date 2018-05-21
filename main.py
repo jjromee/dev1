0a1c64d2583638125cfae5d8fd55fb1b9dfce37f
Juan <juanromero2011@yahoo.com>
2018-05-20 20:46:39 -0500

    final comments added

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from wordList import listOfWords
 from histogram import histo, sortHisto, printMe
 from fileOps import passFile, outputFile
 def main():
-    passed = passFile(sys.argv)
+    passed = passFile(sys.argv) #verify file exists
     if passed == False:
         exit(0)
     else:
-        file = open(sys.argv[1], "r")
-        words = listOfWords(file)
-        hist = histo(words)
-        sortedHist = sortHisto(hist)
-        outputFile(sortedHist)
-        file.close()
+        file = open(sys.argv[1], "r")   #open file
+        words = listOfWords(file)       #make list based on words read in
+        hist = histo(words)             #create dictionary with # occurrences
+        sortedHist = sortHisto(hist)    #sort dictionary based on occurances
+        outputFile(sortedHist)          #print formatted output to output file
+        file.close()                    #close file
 
 main()
